45d10d45d4ba68fffe28df69c42c5c03459fcb53
Sujan Dutta <sujan99dutta@gmail.com>
2019-01-01 12:56:03 +0530

    Update load model and predict.py

--- load model and predict.py
+++ load model and predict.py
@@ -18,12 +18,6 @@ def load_image(img_path, show=True):
         plt.show()
     return img_tensor
 
-'''for img in glob.glob("test*.jpg"):
-    new_image = load_image(img)
-    pred = classifier.predict(new_image)
-    if pred<.5 : print("chat")
-    else : print("not chat")'''
-
 new_image = load_image("test.png")
 pred = classifier.predict(new_image)
 if pred<.5 : print("chat")
